683af2d45e6a6153d95d5acfdb4cfbaf12b6042a
HauseMasterZ <113833707+HauseMasterZ@users.noreply.github.com>
2023-03-09 07:48:24 +0530

    Update Widgets

--- Main.py
+++ Main.py
@@ -1,12 +1,15 @@
+import threading
+import time
 from tkinter import *
 import ctypes
-import subprocess, os, platform
+import subprocess
+import os
+import platform
 import secrets
 from tkinter import filedialog
 import tkinter.messagebox as tkm
 import audioread
 secretsGenerator = secrets.SystemRandom()
-import time, threading
 user32 = ctypes.windll.user32
 screensize = [user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)]
 root = Tk()
@@ -14,13 +17,15 @@ width_screen = int(int(screensize[0])//2)
 height_screen = int(int(screensize[1])//2)
 root.geometry(f"{width_screen}x{height_screen}")
 root.title("True Shuffler")
-
+root.update()
 # Heading
 title = Label(root, text="True Shuffler ~ HauseMaster", font=("Aerial", 13))
-title.pack()
+title.grid(row=0, column=1)
 
 Directory = ""
 dir_musics = []
+
+
 def playAction():
     global dir_musics
     global Directory
@@ -36,14 +41,15 @@ def playAction():
         os.startfile(tmp)
     else:                                   # linux variants
         subprocess.call(('xdg-open', tmp))
-    with audioread.audio_open(tmp) as f:    
+    with audioread.audio_open(tmp) as f:
         totalsec = f.duration
         time.sleep(float(totalsec))
         playAction()
 
 
-
 audio_set = set(["m4a", "mp3", "aac", "webm", "flv"])
+
+
 def openFilePicker():
     global dir_musics
     global Directory
@@ -60,18 +66,22 @@ def openFilePicker():
             dir_list.append(i)
     dir_musics = dir_list[::]
 
+def updateHeight(event):
+    global directory_box
+    directory_box.config(height=directory_box.get('1.0', END).count('\n'))
 
-Select = Button(root, text="Select Music Folder", command= openFilePicker)
-# Select.place(x = 0, y = 48)
-Select.pack(side=LEFT, padx=10)
 
-directory_box = Text(root, fg="black", highlightthickness="1", height=2, width=width_screen//12, bg="yellow")
-# directory_box.place(x = 120, y = 40)
-directory_box.pack(side=LEFT, padx=20)
+Select = Button(root, text="Select Music Folder", command=openFilePicker)
+Select.place(relx=0.01, rely=0.5, anchor=W)
+
+directory_box = Text(root, fg="black", highlightthickness="1",
+                     height=1, bg="yellow")
+directory_box.place(relwidth=0.7, relx=0.5, rely=0.5, anchor=CENTER)
+directory_box.bind("<Key>", updateHeight)
 Directory.replace("/", "//")
-myDown = Button(root, text="True Shuffle!", command = threading.Thread(target=playAction).start, padx=6, pady=10)
-# myDown.place(x = 590, y = 35)
-myDown.pack(side=RIGHT, padx=10)
+
+myDown = Button(root, text="True Shuffle!", command=threading.Thread(
+    target=playAction).start, padx=6, pady=10)
+myDown.place(relx=0.99, rely=0.5, anchor=E)
 
 root.mainloop()
-
